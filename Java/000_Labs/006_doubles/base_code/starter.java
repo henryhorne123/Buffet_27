9a0fb1edf3d4bb7760258ccdd3e8e8e3b7dbe25f
henryhorne123 <henryjoehorne@gmail.com>
2026-09-14 19:00:33 +0000

    update

--- Java/000_Labs/006_doubles/base_code/starter.java
+++ Java/000_Labs/006_doubles/base_code/starter.java
@@ -9,6 +9,11 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		double fahrenheit = 95.0;
+		double celcius = ((5.0/9.0)*(fahrenheit-32.0));
+
+		System.out.println("fahrenheit: "+fahrenheit);
+		System.out.println("celcius: "+celcius);
+
 	}
 }
